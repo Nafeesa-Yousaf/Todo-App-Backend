ca2d1b7d598f828b9da8faa07d3e23b1bd082778
Nafeesa Yousaf <nafeesayousaf2129@gmail.com>
2026-06-04 19:03:49 +0500

    update db url key

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,14 @@
+import os
 import psycopg2 
 from psycopg2 import pool
 from contextlib import contextmanager
 from decouple import config
 
-DATABASE_URL=config("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL") or os.environ.get("POSTGRES_URL") or config("DATABASE_URL", default=None)
+
+if not DATABASE_URL:
+    raise ValueError("Neither DATABASE_URL nor POSTGRES_URL environment variables were found.")
+
 connection_pool=pool.SimpleConnectionPool(
     1,5,DATABASE_URL
 )
@@ -18,3 +23,4 @@ def get_db():
         raise
     finally:
         connection_pool.putconn(conn)
+
